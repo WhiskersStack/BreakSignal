a8250b23b2a67c161ef91d5d49f78f2f85134069
Dan <47456703+WhiskersStack@users.noreply.github.com>
2026-06-05 23:59:59 +0300

    Handle Android back navigation for privacy page

--- android/app/src/main/java/com/whiskersstack/breaksignal/MainActivity.java
+++ android/app/src/main/java/com/whiskersstack/breaksignal/MainActivity.java
@@ -1,5 +1,41 @@
 package com.whiskersstack.breaksignal;
 
+import android.os.Bundle;
+import android.webkit.WebView;
+import androidx.activity.OnBackPressedCallback;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getOnBackPressedDispatcher()
+            .addCallback(
+                this,
+                new OnBackPressedCallback(true) {
+                    @Override
+                    public void handleOnBackPressed() {
+                        WebView webView = getBridge() != null ? getBridge().getWebView() : null;
+
+                        if (webView != null && webView.canGoBack()) {
+                            webView.goBack();
+                            return;
+                        }
+
+                        if (webView != null && isPrivacyPage(webView.getUrl())) {
+                            webView.loadUrl("https://localhost/");
+                            return;
+                        }
+
+                        finish();
+                    }
+                }
+            );
+    }
+
+    private boolean isPrivacyPage(String url) {
+        return url != null && url.startsWith("https://localhost/privacy.html");
+    }
+}
